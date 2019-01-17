3652c961b6dd499683e9e66b422b831a4709ddf8
Pedro <pedrohrdf.ti@gmail.com>
2019-01-16 23:51:00 -0200

    Criando os relacionamentos de banco de dados

--- apps/empresas/models.py
+++ apps/empresas/models.py
@@ -5,3 +5,6 @@ class Empresa(models.Model):
     nome = models.CharField(max_length=100,help_text="Nome da empresa")
 
 
+    def __str__(self):
+        return self.nome
+
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from apps.funcionarios.models import Funcionario
 
 
 class ResgistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100)
-
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
